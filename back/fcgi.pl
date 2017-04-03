d9a625f63e0d14ded327c2acc37288c96788ae92
Dmitry Ovsyanko <do_@pochta.ru>
2017-04-03 21:47:04 +0300

    FCGI config: now in JSON

--- back/fcgi.pl
+++ back/fcgi.pl
@@ -1,10 +1,2 @@
-use Cwd 'abs_path';
-my $path = abs_path ('../../core_cut');
-
-eval qq {
-
-	use lib '$path';
-	use Eludia::Content::HTTP::FCGI::nginx_single;
-	start ();
-
-};
+use Eludia::Content::HTTP::FCGI::single;
+start ();
