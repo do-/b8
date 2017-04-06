ff7bc90724c3e6d7ca3ef08447e369f1277df66f
Dmitry Ovsyanko <do_@pochta.ru>
2017-04-06 21:32:20 +0300

    more client side validation

--- front/root/_/app/js/data/user.js
+++ front/root/_/app/js/data/user.js
@@ -1,8 +1,27 @@
 define ([], function () {
 
-    $_DO.update_user = function () {    
+    $_DO.update_user = function () {
+    
+        var data = values ($('.drw.form'))
+        
+        if (data.password) {
+        
+            if (!data.password2) {
+                $('input[name=password2]').focus ()
+                alert ('Для страховки от опечаток необходимо ввести пароль повторно')
+                return
+            }
 
-        query ({type: 'users', action: 'update'}, {data: values ($('.drw.form'))}, showIt)
+            if (data.password != data.password2) {
+                $('input[type=password]').val ('')
+                $('input[name=password]').focus ()
+                alert ('Ошибка при 1-м или 2-м вводе пароля')
+                return
+            }
+        
+        }
+
+        query ({type: 'users', action: 'update'}, {data: data}, showIt)
 
     }
 
