86ffae9873ac422f60d253153c88949d5b92295f
Dmitry Ovsyanko <do_@pochta.ru>
2017-06-08 16:28:34 +0300

    main: code cosmetics

--- front/root/_/app/js/view/main.js
+++ front/root/_/app/js/view/main.js
@@ -3,10 +3,10 @@ define ([], function () {
     return function (data, view) {
             
         $('title').text ('...')
-                    
-        $('body').empty ().append (view)
         
-        clickOn ($('button.logout'), $_DO.execute_logout)
+        fill (view, {}, $('body'))
+
+        clickOn ($('div.logout button'), $_DO.execute_logout)
         
         use.block ('clock')
         use.block ('auth_toolbar_user')
